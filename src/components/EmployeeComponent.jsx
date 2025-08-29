b6723c1a4b96445fbbd098a4f8562585b3844e82
elleespiel <espielprincessemmanuelle@gmail.com>
2025-08-30 00:37:13 +0800

    Added Form for Add employee function

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EmployeeComponent = () => {
+
+    const [firstName, setFristName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+
+    function handleFirstName(e){
+        setFristName(e.target.value);
+    }
+
+    function handleLastName(e){
+        setLastName(e.target.value);
+    }
+
+    function handleEmail(e){
+        setEmail(e.target.value);
+    }
+
+    function saveEmployee(e){
+        e.preventDefault();
+
+        const employee = {firstName, lastName, email}
+
+        console.log(employee)
+    }
+
   return (
-    <div>E</div>
+    <div className='container'>
+        <br />
+        <div className='row'>
+            <div className='card col-md-6 offset-md-3 offset-md-3'>
+                <h2 className='text-center'>Add Employee</h2>
+                <div className='card-body'>
+                    <form>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>First Name:</label>
+                            <input
+                                type='text'
+                                placeholder='Emter employee first name'
+                                name='firstname'
+                                value={firstName}
+                                className='form-control'
+                                onChange={handleFirstName}
+                            ></input>
+                        </div>
+
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Last Name:</label>
+                            <input
+                                type='text'
+                                placeholder='Emter employee last name'
+                                name='lastname'
+                                value={lastName}
+                                className='form-control'
+                                onChange={handleLastName}
+                            ></input>
+                        </div>
+
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Email:</label>
+                            <input
+                                type='text'
+                                placeholder='Emter employee email'
+                                name='email'
+                                value={email}
+                                className='form-control'
+                                onChange={handleEmail}
+                            ></input>
+                        </div>
+
+                        <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
